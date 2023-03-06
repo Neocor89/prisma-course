81aaa71f20acb37708ccc226aced48c7f5c7839a
Ben <ncor89b@gmail.com>
2023-03-07 00:55:09 +0100

    feat: implementation of DB auto disconnection and start create queries

--- script.ts
+++ script.ts
@@ -1,2 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+async function main() {
+  //: Write Prisma Client Queries
+}
+
+main()
+
+.catch(e => {
+  console.error(e.message);
+})
+.finally(async () => {
+  //: Disconnect from the database after queries running
+  await prisma.$disconnect()
+})
