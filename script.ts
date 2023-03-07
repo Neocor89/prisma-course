e12bdc3d3fe3b3f12b65636ab431aa27085c8ff4
Ben <ncor89b@gmail.com>
2023-03-07 22:02:14 +0100

    feat: create users and adding to database

--- script.ts
+++ script.ts
@@ -3,14 +3,29 @@ const prisma = new PrismaClient();
 
 async function main() {
   //: Write Prisma Client Queries
+
+  /*
+  const user = await prisma.user.create({
+    data: {
+      name: "Neocor89"
+    }
+  })
+  console.log(user);
+  */
+
+  const users = await prisma.user.findMany();
+  console.log(users);
+  
+
+  
 }
 
 main()
 
+//: Disconnect database after queries running
 .catch(e => {
   console.error(e.message);
 })
 .finally(async () => {
-  //: Disconnect from the database after queries running
   await prisma.$disconnect()
 })
